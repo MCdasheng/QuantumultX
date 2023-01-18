609d249cd7d233f3b6566f854b8b9677d4087425
MCdasheng <92009557+MCdasheng@users.noreply.github.com>
2023-01-18 09:31:11 +0800

    Create test.js

---
+++ Scripts/test.js
@@ -0,0 +1,21 @@
+/*
+[rewrite_local]
+^http[s]s:\/\/cdn\.alzhuanxian1\.com:7390\/platform-ns\/v1\.0\/member\/money url script-response-body test.js
+[mitm] 
+hostname = cdn.alzhuanxian1.com
+*/
+
+if ($response.body) {
+    $done({
+        body: JSON.stringify({
+  "msg" : "成功",
+  "data" : {
+    "total_account" : 100
+  },
+  "code" : 0
+}
+)
+    });
+} else {
+    $done({});
+}
