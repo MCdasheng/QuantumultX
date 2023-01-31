597c2806145fc83fffdb0d13ffc6bc74580592eb
MCdasheng <92009557+MCdasheng@users.noreply.github.com>
2023-01-31 12:22:59 +0800

    Create Emby.js

---
+++ Scripts/Emby.js
@@ -0,0 +1,30 @@
+/*
+自用备份 Emby Premiere
+
+[Rewrite Local]
+^https?:\/\/mb3admin.com\/admin\/service\/registration\/validateDevice url script-response-body https://raw.githubusercontent.com/Tartarus2014/Script/master/Emby.js
+
+[Hostname]
+hostname = mb3admin.com
+*/
+
+const CHECK_URL = 'mb3admin.com/admin/service/registration/validateDevice'
+
+const url = $request.url
+const isCheckUrl = (url) => url.includes(CHECK_URL)
+
+if (isCheckUrl(url) && $response.status != 200) {
+  const unlock = {
+    cacheExpirationDays: 999,
+    resultCode: 'GOOD',
+    message: 'Device Valid'
+  }
+
+  const status = 200
+  const headers = $response.headers
+  const body = JSON.stringify(unlock)
+
+  $done({ status, headers, body })
+} else {
+  $done({})
+}
