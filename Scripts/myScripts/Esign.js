70feb7e7c346289c9b7902cc2e81fe0053bfd439
MCdasheng <92009557+MCdasheng@users.noreply.github.com>
2023-03-12 13:31:09 +0800

    Update Esign.js

--- Scripts/myScripts/Esign.js
+++ Scripts/myScripts/Esign.js
@@ -4,7 +4,7 @@
 30 10 * * * https://raw.githubusercontent.com/MCdasheng/QuantumultX/main/Scripts/myScripts/Esign.js, tag=轻松签版本监控, img-url=https://raw.githubusercontent.com/MCdasheng/QuantumultX/main/Icon/ESign.png, enabled=true
 */
 
-const currentVerision = $prefs.valueForKey("currentVerision");
+const currentVersion = $prefs.valueForKey("currentVersion");
 const url = "https://esign.yyyue.xyz/yyy/api/esignapp/lastVersion";
 const method = "GET";
 const headers = {
@@ -23,7 +23,7 @@ const myRequest = {
 $task.fetch(myRequest).then(
   (response) => {
     const obj = JSON.parse(response.body);
-    if (obj.result.version !== currentVerision) {
+    if (obj.result.version !== currentVersion) {
       const notice = "👇New version has released!\n" + obj.result.lzurl;
       $notify("轻松签版本监控", "", notice);
       console.log(notice);
