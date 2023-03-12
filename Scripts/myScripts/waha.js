42930c0f969b175bae2f754c1108bee685b98312
MCdasheng <92009557+MCdasheng@users.noreply.github.com>
2023-03-12 11:54:40 +0800

    Update waha.js

--- Scripts/myScripts/waha.js
+++ Scripts/myScripts/waha.js
@@ -1,20 +1,22 @@
 /*
 脚本功能: 哇哈体育签到
-29 10 * * * https://raw.githubusercontent.com/MCdasheng/QuantumultX/main/Scripts/myScripts/waha.js, tag=哇哈体育签到
+29 10 * * * https://raw.githubusercontent.com/MCdasheng/QuantumultX/main/Scripts/waha.js, tag=哇哈体育签到
 */
 
-const url =
-  "https://bbs.wahatiyu.com/plugin.php?id=gsignin:index&action=signin&formhash=dca5dc70";
-const method = "GET";
-const data = { info: "abc" };
-
 let cookie = $prefs.valueForKey("cookie");
+if (!cookie) {
+  $notify("Cookie为空,请检查cookie!");
+}
 let headers = {
   "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.63",
   Cookie: cookie,
 };
 
+const url =
+  "https://bbs.wahatiyu.com/plugin.php?id=gsignin:index&action=signin&formhash=dca5dc70";
+const method = "GET";
+const data = { info: "abc" };
 const myRequest = {
   url: url,
   method: method,
