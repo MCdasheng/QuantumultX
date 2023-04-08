48bf0319952bf0b5e259c520c2531606cea28110
MCdasheng <92009557+MCdasheng@users.noreply.github.com>
2023-04-09 01:51:04 +0800

    Update bfy.js

--- Scripts/myScripts/bfy.js
+++ Scripts/myScripts/bfy.js
@@ -1,26 +1,23 @@
 /*
-脚本功能: 获取八方云试用订阅
-注: 3h有效; 关闭代理运行
+脚本功能: 获取八方云试用订阅(3h有效)
+注: 关闭代理运行
 33 10 * * * https://raw.githubusercontent.com/MCdasheng/QuantumultX/main/Scripts/myScripts/bfy.js, tag=八方云订阅, img-url=https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Final.png, enabled=true
 */
+
+const auth = "bfy_auth";
+const tagName = "八方云";
+const register_url = `https://bafangyun.vip/api/v1/passport/auth/register`;
+const subscribe_url = `https://bafangyun.vip/api/v1/user/getSubscribe`;
 
 a();
 
 function a() {
   const rd = Math.random().toString(36).slice(-8);
-  const url = `https://bafangyun.vip/api/v1/passport/auth/register`;
+  const url = register_url;
   const method = `POST`;
   const headers = {
-    Accept: `*/*`,
-    Origin: `https://bafangyun.vip`,
-    "Accept-Encoding": `gzip, deflate, br`,
-    "Content-Language": `zh-CN`,
     "Content-Type": `application/x-www-form-urlencoded`,
-    Host: `bafangyun.vip`,
-    Connection: `keep-alive`,
     "User-Agent": `Mozilla/5.0 (iPhone; CPU iPhone OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Mobile/15E148 Safari/604.1`,
-    Referer: `https://bafangyun.vip/`,
-    "Accept-Language": `zh-CN,zh-Hans;q=0.9`,
   };
   const body = `email=${rd}%40qq.com&password=12345678&invite_code=&email_code=`;
 
@@ -35,11 +32,11 @@ function a() {
     (response) => {
       //   console.log(response.body);
       var obj = JSON.parse(response.body);
-      var bfy_auth = obj.data.auth_data;
-      $prefs.setValueForKey(bfy_auth, "bfy_auth");
-      if ($prefs.valueForKey("bfy_auth") == bfy_auth) {
+      var AUTH = obj.data.auth_data;
+      $prefs.setValueForKey(AUTH, `${auth}`);
+      if ($prefs.valueForKey(`${auth}`) == AUTH) {
         console.log("🎉注册成功!");
-        console.log(bfy_auth);
+        console.log(AUTH);
       } else {
         console.log("🔴注册失败!");
         console.log(response.body);
@@ -56,18 +53,11 @@ function a() {
 }
 
 function b() {
-  const url = `https://bafangyun.vip/api/v1/user/getSubscribe`;
+  const url = subscribe_url;
   const method = `GET`;
   const headers = {
-    "Accept-Encoding": `gzip, deflate, br`,
-    Accept: `*/*`,
-    Connection: `keep-alive`,
-    "Content-Language": `zh-CN`,
-    Referer: `https://bafangyun.vip/`,
-    Host: `bafangyun.vip`,
     "User-Agent": `Mozilla/5.0 (iPhone; CPU iPhone OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Mobile/15E148 Safari/604.1`,
-    "Accept-Language": `zh-CN,zh-Hans;q=0.9`,
-    Authorization: $prefs.valueForKey("bfy_auth"),
+    Authorization: $prefs.valueForKey(`${auth}`),
   };
   const body = ``;
 
@@ -84,9 +74,10 @@ function b() {
       var obj = JSON.parse(response.body);
       if (obj.data.subscribe_url) {
         var url = obj.data.subscribe_url;
-        $notify("八方云订阅", "🎉获取订阅链接成功!", url);
+        var sub = `${url}#emoji=2, tag=${tagName}, opt-parser=true, enabled=true`;
         console.log("🎉订阅获取成功!");
-        console.log(`${url}, tag=八方云, opt-parser=true, enabled=true`);
+        console.log(sub);
+        $notify(`${tagName}订阅`, "🎉获取订阅链接成功!", sub);
         $done();
       } else {
         console.log("🔴订阅获取失败!");
