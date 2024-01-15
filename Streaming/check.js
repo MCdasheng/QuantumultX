f414e49a3334528c89bdc5b707f656ae4449d941
MCdasheng <305628252@qq.com>
2024-01-15 23:57:42 +0800

    fix

--- Streaming/check.js
+++ Streaming/check.js
@@ -495,6 +495,9 @@ async function getIp() {
         "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
       "Accept-Encoding": "gzip, deflate, br",
     },
+    opts: {
+      policy: $environment.params,
+    },
   };
 
   return $.http.get(options).then((resp) => {
